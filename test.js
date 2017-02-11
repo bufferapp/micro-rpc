19e82a5cdf25f051f3c04473c05043d89119d888
Harrison Harnisch <hharnisc@gmail.com>
2017-02-10 18:03:35 -0600

    add test case for sync and async code

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
 const MicroRPC = require('./index');
 const { server, method } = MicroRPC();
 
-method('add', (a, b) => new Promise((resolve) => {
+method('add', (a, b) => a + b);
+
+method('addAsync', (a, b) => new Promise((resolve) => {
   resolve(a + b);
 }));
 
-method('addItems', ({ a, b }) => new Promise((resolve) => {
+
+method('addItems', ({ a, b }) => a + b);
+
+method('addItemsAsync', ({ a, b }) => new Promise((resolve) => {
   resolve(a + b);
 }));
+
 
 server.listen(3000);
